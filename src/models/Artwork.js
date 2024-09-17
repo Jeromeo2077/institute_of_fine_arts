0c1b6dea60085326709cd1ea32cec5b1aad7dab1
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-17 15:50:13 -0600

    Entered properties into the Artwork model, saved the API response into the AppState

---
+++ src/models/Artwork.js
@@ -0,0 +1,8 @@
+export class Artwork {
+  constructor(data){
+    this.id = data.id
+    this.imgUrls = data.imgUrls.orginalLink
+    this.description = data.description
+  }
+
+}
--- src/services/ArtworkService.js
+++ src/services/ArtworkService.js
@@ -1,13 +1,15 @@
+import { Artwork } from "@/models/Artwork.js"
 import { api } from "./AxiosService.js"
+import { AppState } from "@/AppState.js"
 
 class ArtworkService{
 
   async getArtwork() {
     const response = await api.get('api/artworks')
-    console.log(response.data);
-    
+   
+    const newArtwork = response.data.map(artPojo => new Artwork(artPojo))
+    AppState.artwork = newArtwork   
   }
-
 }
 
-export const artworkService = new ArtworkService
+export const artworkService = new ArtworkService()
